73b31a0378a1b5b66a148366cd8863cd03b0da63
Claire Cavanaugh <LadyClaire@users.noreply.github.com>
2016-05-02 12:17:01 -0700

    Update mandelbrot2.py
    Fractal now persists until key is pressed

--- mandelbrot2.py
+++ mandelbrot2.py
@@ -1,6 +1,6 @@
 # Renders the Mandelbrot Set in ASCII art. This version of the code
 # runs with Python 2. 
-# Licensed under WTFL, Claire Cavanaugh, 2016
+# Claire Cavanaugh, 2016
 # With special thanks to pjdelport for her help with optimizing.
 
 
@@ -50,7 +50,8 @@ def main(stdscr):
             c = complex(x, y)
             stdscr.addstr(row, col, mandelbrotTest(c))
         stdscr.refresh()
-
+    
+    stdscr.getkey()
 
 
 if __name__ == "__main__":
